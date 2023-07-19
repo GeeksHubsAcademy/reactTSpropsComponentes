c12e235c56d66aa35b3171654d96a52d8376a632
Dave86dev <davidochando1986@gmail.com>
2023-07-19 12:52:05 +0200

    Redux remove fixed

--- src/pages/cartSlice.ts
+++ src/pages/cartSlice.ts
@@ -17,7 +17,7 @@ export const cartSlice = createSlice({
       remove: (state, action) => {
         return {
           ...state,
-          items: []
+          items: action.payload
         }
       }
       
