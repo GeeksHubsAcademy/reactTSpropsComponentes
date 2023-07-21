425a23707457843e26aada2244a9ff06d450962d
Dave86dev <davidochando1986@gmail.com>
2023-07-21 10:34:00 +0200

    Login half done no validation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login, userData } from "../userSlice";
 
-import './Login.css';
+import "./Login.css";
 
 export interface Credentials {
   email: string;
@@ -22,7 +22,6 @@ export const Login = () => {
 
   //Conecto a REDUX en modo escritura
   const dispatch = useDispatch();
-  
 
   const [userCredentials, setUserCredentials] = useState<Credentials>({
     email: "",
@@ -31,41 +30,42 @@ export const Login = () => {
 
   const [userError, setUserError] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
   const [welcome, setWelcome] = useState<string>("");
 
-  useEffect(()=> {
-    if(reduxUserCredentials.credentials?.token){
+  useEffect(() => {
+    if (reduxUserCredentials.credentials?.token) {
       navigate("/");
     }
-  },[]);
+  }, []);
 
-  useEffect(()=> {
+  // useEffect(()=> {
 
-    if(userCredentials.email !== ""){
+  //   if(userCredentials.email !== ""){
 
-        console.log(userCredentials)
-    }
+  //       console.log(userCredentials)
+  //   }
 
-  }, [userCredentials])
+  // }, [userCredentials])
 
   const submitHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+
     e.preventDefault();
 
     logMe(userCredentials)
       .then((res) => {
-        console.log(res);
+      
+          //Guardo en Redux
+          dispatch(login({ credentials: res }));
 
-        //Guardo en Redux
-        dispatch(login({ credentials: res}))
+          setWelcome(res?.name);
 
-        setWelcome(res.name);
-
-        setTimeout(() => {
-          navigate("/");
-        }, 750);
+          setTimeout(() => {
+            navigate("/")
+          }, 750);
+        
       })
       .catch((error) => console.log(error));
   };
@@ -76,8 +76,19 @@ export const Login = () => {
         {welcome !== "" ? (
           <>
             <Row className="justify-content-center">
-              <Col className="d-flex flex-column justify-content-center" xs={10} md={4} xl={3}>
-                Bienvenido de nuevo {welcome}....
+              <Col
+                className="d-flex flex-column justify-content-center"
+                xs={10}
+                md={4}
+                xl={3}
+              >
+                {
+                  welcome !== "Las credenciales de Login no son correctas" &&
+
+                  <>
+                    Bienvenido de nuevo {welcome}....
+                  </>
+                }
               </Col>
             </Row>
           </>
@@ -94,20 +105,28 @@ export const Login = () => {
                   name="email"
                   type="email"
                   placeholder="write an email..."
-                  design={userError.email ? ("my-4 inputDesign errorInput") : ("my-4 inputDesign")}
+                  design={
+                    userError.email
+                      ? "my-4 inputDesign errorInput"
+                      : "my-4 inputDesign"
+                  }
                   state={setUserCredentials}
                   errorState={setUserError}
                 />
-                <div className='msgError'>{userError.email}</div>
+                <div className="msgError">{userError.email}</div>
                 <TextInput
                   name="password"
                   type="password"
                   placeholder="write a password..."
-                  design={userError.password ? ("my-4 inputDesign errorInput") : ("my-4 inputDesign")}
+                  design={
+                    userError.password
+                      ? "my-4 inputDesign errorInput"
+                      : "my-4 inputDesign"
+                  }
                   state={setUserCredentials}
                   errorState={setUserError}
                 />
-                <div className='msgError'>{userError.password}</div>
+                <div className="msgError">{userError.password}</div>
                 <Button onClick={(e) => submitHandler(e)}>log me!</Button>
               </Col>
             </Row>
--- src/services/apiCalls.ts
+++ src/services/apiCalls.ts
@@ -10,11 +10,11 @@ interface LoginData {
 }
 
 interface RegisterData {
-  id: number,
-  name: string,
-  surname: string, 
-  age: number,
-  userId: number
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  userId: number;
 }
 
 export const bringCharacters = async () => {
@@ -23,72 +23,68 @@ export const bringCharacters = async () => {
   return data.results;
 };
 
-export const bringCharactersName = async (name:any) => {
-
-  let { data } = await axios.get(`${RM_URL}/character/?name=${name.search}`)
+export const bringCharactersName = async (name: any) => {
+  let { data } = await axios.get(`${RM_URL}/character/?name=${name.search}`);
 
   return data.results;
-}
+};
 
-export const themoviedbExample = async (query:any) => {
-  
+export const themoviedbExample = async (query: any) => {
   //Ejemplo de como hacer llamadas a la API themoviedb según su esquema en la web
   const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&page=1`;
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer 210d6a5dd3f16419ce349c9f1b200d6d'
-    }
+      accept: "application/json",
+      Authorization: "Bearer 210d6a5dd3f16419ce349c9f1b200d6d",
+    },
   };
 
   let { data } = await axios.get(url, options);
 
   return data.results;
-}
+};
 
 export const logMe = async (body: LoginData) => {
-  // let res = await axios.post(`${URL}/auth/login`, body);
+  //En este primer paso me traigo a todos los usuarios de la bd.
+  let res = await axios.get(`${BASIC_API_URL}/users`);
 
-  let res = {
-    token: "askdlmfaskldmfasñlkdfmaslñdkfmaslñdkfm",
-    name: "David",
-    id: 5,
-    age: 37,
-    rol: true
-  }
+  let found = {};
 
-  return res;
+  res.data.map(user => {
+    if (user.email === body.email && user.password === body.password) {
+      //Si entramos en este condicional, es que las credenciales son correctas..
+      found = user;
+    }
+  });
+
+  return found;
+
 };
 
 export const registerMe = async (body: RegisterData) => {
-
   let { data } = await axios.post(`${BASIC_API_URL}/users`, body);
 
   return data;
-
-}
+};
 
 export const profileUser = async (id: string) => {
-
   let { data } = await axios.get(`${BASIC_API_URL}/users/${id}`);
 
   return data;
-}
+};
 
 export const newOrder = async (product_id: string, user_id: string) => {
-
   console.log(product_id);
 
   let body = {
     id: 2,
     user_id: user_id,
     product_id: product_id,
-    date: "20/07/2023"
-  }
+    date: "20/07/2023",
+  };
 
   let { data } = await axios.post(`${BASIC_API_URL}/orders`, body);
 
   return data;
-
-}
+};
